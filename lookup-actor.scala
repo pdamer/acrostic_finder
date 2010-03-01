c15299cd151ad93c7572d1dfecebc1931aa8e883
Paul Damer <paul.damer@duke.edu>
2010-03-01 10:05:33 -0500

    making lookup a little neater

--- lookup-actor.scala
+++ lookup-actor.scala
@@ -2,17 +2,22 @@
 package acrostic {
   import scala.actors._
   import scala.actors.Actor._
-  
+  import scala.util.matching.Regex
+
   case class Match(word: String, column: Int, line: Int)
 
-  class Lookup(word: String, outputter: Actor) extends Actor {
-  
+  class Lookup(word_reg: Regex, outputter: Actor) extends Actor {
+    
+    def this(word: String, outputter: Actor) = 
+      this(word.r, outputter)
+
+
     def act() {
       loop {
         react {
           case c: Column => {
-            for (found_word <- word.r.findAllIn(c.text).matchData) {
-              outputter! new Match(word, c.index, found_word.start)
+            for (found_word <- word_reg.findAllIn(c.text).matchData) {
+              outputter! new Match(found_word.toString, c.index, found_word.start)
             }
           }
           case x: Any   => println("Lookup Error: Unknown message! " + x)
